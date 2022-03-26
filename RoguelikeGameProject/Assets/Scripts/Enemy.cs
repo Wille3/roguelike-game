94c5d725ff64333a35c040bbb79423c00955eaa7
Wille3 <willearv03@gmail.com>
2022-03-26 19:03:36 +0100

    Changed outline color and added enemies

---
+++ RoguelikeGameProject/Assets/Scripts/Enemy.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 3f;
+    private Transform target;
+
+    private void Update()
+    {
+        if (target != null)
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards
+                (transform.position, target.position, step);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            target = other.transform;
+        }
+    }
+}
+
